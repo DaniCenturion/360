f59b37d2324ac164f7e748c39e44f44dc06d21bb
DaniCenturion <77085633+DaniCenturion@users.noreply.github.com>
2022-02-26 13:33:05 -0300

    Se elimino UsuariosDAOIMP, Se agregó Eventos DAOIMP y se modifico MenuSistema como prueba

--- src/java/Modelo/DaoImp/usuariosDAOIMP.java
+++ src/java/Modelo/DaoImp/usuariosDAOIMP.java
@@ -1,36 +1,40 @@
 package Modelo.DaoImp;
 
-import Modelo.Dao.MenuSistemaDAO;
-import Modelo.Dto.MenuSistemaDTO;
+import Modelo.Dao.UsuarioDAO;
+import Modelo.Dto.PerfilDTO;
+import Modelo.Dto.PersonaDTO;
+import Modelo.Dto.UsuarioDTO;
 import base.Conexion;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class usuariosDAOIMP implements MenuSistemaDAO {
+public class UsuariosDAOIMP implements UsuarioDAO {
 
     private String sql;
     private Conexion conexion;
     private PreparedStatement ps;
     private ResultSet rs;
 
-    public usuariosDAOIMP() {
+    public UsuariosDAOIMP() {
         conexion = new Conexion();
 
     }
 
     @Override
-    public boolean agregarRegistro(MenuSistemaDTO dto) {
+    public boolean agregarRegistro(UsuarioDTO dto) {
         try {
             conexion.Transaccion(Conexion.TR.INICIAR);
-            sql = "INSERT INTO menu_sistema(descrip, comentario) VALUES ( ?, ?);";
+            sql = "INSERT INTO usuarios( usuario, clave, id_perfil, id_persona) VALUES (?, ?, ?, ?);";
             ps = conexion.obtenerConexion().prepareStatement(sql);
-            ps.setString(1, dto.getDescrip());
-            ps.setString(2, dto.getComentario());
+            ps.setString(1, dto.getUsuario());
+            ps.setString(2, dto.getClave());
+            ps.setInt(3, dto.getPerfil().getId());
+            ps.setInt(4, dto.getPersona().getId());
+
             if (ps.executeUpdate() > 0) {
                 conexion.Transaccion(Conexion.TR.CONFIRMAR);
             } else {
@@ -40,7 +44,7 @@ public class usuariosDAOIMP implements MenuSistemaDAO {
 
             return false;
         } catch (SQLException ex) {
-            Logger.getLogger(usuariosDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(MenuSistemaDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
             conexion.Transaccion(Conexion.TR.CANCELAR);
             return false;
         } finally {
@@ -48,20 +52,23 @@ public class usuariosDAOIMP implements MenuSistemaDAO {
                 conexion.cerrarConexion();
                 ps.close();
             } catch (SQLException ex) {
-                Logger.getLogger(usuariosDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(MenuSistemaDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
 
     @Override
-    public boolean modificarRegistro(MenuSistemaDTO dto) {
+    public boolean modificarRegistro(UsuarioDTO dto) {
         try {
             conexion.Transaccion(Conexion.TR.INICIAR);
-            sql = "UPDATE menu_sistema SET descrip=?, comentarios=? WHERE id=?;";
+            sql = "UPDATE usuarios SET usuarioo=?, clave=?, id_perfil=?, id_persona=? WHERE id_usuario=?;";
             ps = conexion.obtenerConexion().prepareStatement(sql);
-            ps.setString(1, dto.getDescrip());
-            ps.setString(2, dto.getComentario());
-            ps.setInt(3, dto.getId());
+            ps.setString(1, dto.getUsuario());
+            ps.setString(2, dto.getClave());
+            ps.setInt(3, dto.getPerfil().getId());
+            ps.setInt(4, dto.getPersona().getId());
+            ps.setInt(5, dto.getId());
+
             if (ps.executeUpdate() > 0) {
                 conexion.Transaccion(Conexion.TR.CONFIRMAR);
             } else {
@@ -71,7 +78,7 @@ public class usuariosDAOIMP implements MenuSistemaDAO {
 
             return false;
         } catch (SQLException ex) {
-            Logger.getLogger(usuariosDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(MenuSistemaDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
             conexion.Transaccion(Conexion.TR.CANCELAR);
             return false;
         } finally {
@@ -79,18 +86,20 @@ public class usuariosDAOIMP implements MenuSistemaDAO {
                 conexion.cerrarConexion();
                 ps.close();
             } catch (SQLException ex) {
-                Logger.getLogger(usuariosDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(MenuSistemaDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+
     }
 
     @Override
-    public boolean eliminarRegistro(MenuSistemaDTO dto) {
+    public Boolean inactivarUsuario(Integer id) {
         try {
             conexion.Transaccion(Conexion.TR.INICIAR);
-            sql = "DELETE FROM menu_sistema WHERE id=?;";
+            sql = "UPDATE usuarios SET estado = ? WHERE id_usuario=?;";
             ps = conexion.obtenerConexion().prepareStatement(sql);
-            ps.setInt(1, dto.getId());
+            ps.setBoolean(1, false);
+            ps.setInt(2, id);
             if (ps.executeUpdate() > 0) {
                 conexion.Transaccion(Conexion.TR.CONFIRMAR);
             } else {
@@ -100,7 +109,7 @@ public class usuariosDAOIMP implements MenuSistemaDAO {
 
             return false;
         } catch (SQLException ex) {
-            Logger.getLogger(usuariosDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(MenuSistemaDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
             conexion.Transaccion(Conexion.TR.CANCELAR);
             return false;
         } finally {
@@ -108,28 +117,32 @@ public class usuariosDAOIMP implements MenuSistemaDAO {
                 conexion.cerrarConexion();
                 ps.close();
             } catch (SQLException ex) {
-                Logger.getLogger(usuariosDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(MenuSistemaDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
 
     @Override
-    public MenuSistemaDTO recuperarRegistro(Integer id) {
+    public UsuarioDTO recuperarRegistro(Integer id) {
         try {
-            MenuSistemaDTO dto = null;
-            sql = "SELECT id, descrip, comentario FROM menu_sistema WHERE id= ?;";
+            UsuarioDTO dto = null;
+            sql = "SELECT A.id_usuario, A.usuario, A.id_perfil, B.descripcion AS perfil, id_persona, C.nombres, C.apellidos AS persona\n"
+                    + "FROM usuarios A  INNER JOIN perfil B ON A.id_perfil = B.id\n"
+                    + "                 INNER JOIN personas C ON A.id_persona = C.id\n"
+                    + "WHERE id_usuario = ? AND estado = true";
             ps = conexion.obtenerConexion().prepareStatement(sql);
             ps.setInt(1, id);
             rs = ps.executeQuery();
             if (rs.next()) {
-                dto = new MenuSistemaDTO();
+                dto = new UsuarioDTO();
                 dto.setId(rs.getInt("id"));
-                dto.setDescrip(rs.getString("descrip"));
-                dto.setComentario(rs.getString("comentario"));
+                dto.setUsuario(rs.getString("usuario"));
+                dto.setPerfil( new PerfilDTO(rs.getInt("id_perfil"), rs.getString("perfil")));
+                dto.setPersona(new PersonaDTO(rs.getInt("id_persona"), rs.getString("nombres"), rs.getString("apellidos")));
             }
             return dto;
         } catch (SQLException ex) {
-            Logger.getLogger(usuariosDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(MenuSistemaDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
             return null;
         } finally {
             try {
@@ -137,40 +150,19 @@ public class usuariosDAOIMP implements MenuSistemaDAO {
                 ps.close();
                 rs.close();
             } catch (SQLException ex) {
-                Logger.getLogger(usuariosDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
+                Logger.getLogger(MenuSistemaDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
 
     @Override
-    public List<MenuSistemaDTO> recuperarRegistros() {
-        try {
-            List<MenuSistemaDTO> lista = null;
-            MenuSistemaDTO dto = null;
-            sql = "SELECT id, descrip, comentario FROM menu_sistema ";
-            ps = conexion.obtenerConexion().prepareStatement(sql);
-            rs = ps.executeQuery();
-            lista = new ArrayList<>();
-            while (rs.next()) {
-                dto = new MenuSistemaDTO();
-                dto.setId(rs.getInt("id"));
-                dto.setDescrip(rs.getString("descrip"));
-                dto.setComentario(rs.getString("comentario"));
-                lista.add(dto);
-            }
-            return lista;
-        } catch (SQLException ex) {
-            Logger.getLogger(usuariosDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        } finally {
-            try {
-                conexion.cerrarConexion();
-                ps.close();
-                rs.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(usuariosDAOIMP.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+    public List<UsuarioDTO> recuperarRegistros() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminarRegistro(UsuarioDTO dto) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
